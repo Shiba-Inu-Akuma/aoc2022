759b7e668f5fe97e113d608ca6705d61d56c524b
Atilla Atmaca <e1425630@student.tuwien.ac.at>
2022-12-29 22:42:04 +0100

    Part 2 of Day 02 completed

--- src/Day_02/RockPaperScissors.java
+++ src/Day_02/RockPaperScissors.java
@@ -12,12 +12,13 @@ public class RockPaperScissors {
     private static String myCode = "XYZ";
 
     public static void main(String[] args) throws IOException {
-        System.out.println(getMyTotalScore(Paths.get("resources/Day_02.txt")));
+        System.out.println(getMyScoreStratA(Paths.get("resources/Day_02.txt")));
+        System.out.println(getMyScoreStratB(Paths.get("resources/Day_02.txt")));
     }
 
 
     // Part 1
-    public static int getMyTotalScore(Path path) throws IOException {
+    public static int getMyScoreStratA (Path path) throws IOException {
         List<String> input = Files.readAllLines(path);
         int myScore = 0;
 
@@ -31,7 +32,29 @@ public class RockPaperScissors {
             else if (myResponse == 0 && opponentResponse == 2) myScore += 6;
             else if (myResponse > opponentResponse && myResponse + opponentResponse != 2) myScore += 6;
         }
+        return myScore;
+    }
 
+    // Part 2
+    public static int getMyScoreStratB (Path path) throws IOException {
+        List<String> input = Files.readAllLines(path);
+        int myScore = 0;
+
+        for (String line : input) {
+            int myResponse = myCode.indexOf(line.split(" ")[1]);
+            int opponentResponse = opponentCode.indexOf(line.split(" ")[0]);
+
+            if (myResponse == 1) myScore += 3 + opponentResponse + 1;
+            else if (myResponse == 0) {
+                if (opponentResponse == 0) myScore += 3;
+                else myScore += opponentResponse;
+            }
+            else {
+                myScore += 6;
+                if (opponentResponse == 2) myScore += 1;
+                else myScore += opponentResponse + 2;
+            }
+        }
         return myScore;
     }
 }
