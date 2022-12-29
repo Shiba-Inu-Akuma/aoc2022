bb9109d214be870349b01ae7ef7f50a614be24e3
Atilla Atmaca <e1425630@student.tuwien.ac.at>
2022-12-29 22:25:10 +0100

    Part 1 of Day 02 completed

--- src/Day_02/RockPaperScissors.java
+++ src/Day_02/RockPaperScissors.java
@@ -1,8 +1,37 @@
 package Day_02;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class RockPaperScissors {
 
-    public static void main(String[] args) {
+    private static String opponentCode = "ABC";
+    private static String myCode = "XYZ";
 
+    public static void main(String[] args) throws IOException {
+        System.out.println(getMyTotalScore(Paths.get("resources/Day_02.txt")));
+    }
+
+
+    // Part 1
+    public static int getMyTotalScore(Path path) throws IOException {
+        List<String> input = Files.readAllLines(path);
+        int myScore = 0;
+
+        for (String line : input) {
+            int myResponse = myCode.indexOf(line.split(" ")[1]);
+            int opponentResponse = opponentCode.indexOf(line.split(" ")[0]);
+
+            myScore += myResponse + 1;
+
+            if (myResponse == opponentResponse) myScore += 3;
+            else if (myResponse == 0 && opponentResponse == 2) myScore += 6;
+            else if (myResponse > opponentResponse && myResponse + opponentResponse != 2) myScore += 6;
+        }
+
+        return myScore;
     }
 }
