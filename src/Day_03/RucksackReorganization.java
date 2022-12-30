458a142278014a572a858bf31fe9477582b4788e
Atilla Atmaca <e1425630@student.tuwien.ac.at>
2022-12-30 23:43:25 +0100

    Part 2 of Day 03 completed

--- src/Day_03/RucksackReorganization.java
+++ src/Day_03/RucksackReorganization.java
@@ -10,9 +10,10 @@ public class RucksackReorganization {
 
     public static void main(String[] args) throws IOException {
         System.out.println(getSumOfPriorities(Paths.get("resources/Day_03.txt")));
+        System.out.println(getSumOfAuthPriorities(Paths.get("resources/Day_03.txt")));
     }
 
-
+    // Part 1
     public static int getSumOfPriorities(Path path) throws IOException {
         List<String> input = Files.readAllLines(path);
         int sum = 0;
@@ -29,6 +30,25 @@ public class RucksackReorganization {
                 }
             }
 
+        }
+        return sum;
+    }
+
+
+    // Part 2
+    public static int getSumOfAuthPriorities(Path path) throws IOException {
+        List<String> input = Files.readAllLines(path);
+        int sum = 0;
+
+        for (int line = 0; line < input.size(); line += 3) {
+            String rucksack = input.get(line);
+            for (int i = 0; i < rucksack.length(); i++) {
+                if (input.get(line + 1).contains(rucksack.charAt(i) + "") && input.get(line + 2).contains(rucksack.charAt(i) + "")) {
+                    if (rucksack.charAt(i) <= 'Z') sum += rucksack.charAt(i) - ('A' - 27);
+                    else sum += rucksack.charAt(i) - ('a' - 1);
+                    break;
+                }
+            }
         }
         return sum;
     }
