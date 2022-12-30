a7106d150bf245a19ddf6da0408fb7e25b8b0694
Atilla Atmaca <e1425630@student.tuwien.ac.at>
2022-12-30 23:32:43 +0100

    Part 1 of Day 03 completed

--- src/Day_03/RucksackReorganization.java
+++ src/Day_03/RucksackReorganization.java
@@ -3,15 +3,13 @@ package Day_03;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 public class RucksackReorganization {
 
-    public static void main(String[] args) {
-        System.out.println('a');
-        System.out.println('b');
-        System.out.println('z');
-        System.out.println('A');
+    public static void main(String[] args) throws IOException {
+        System.out.println(getSumOfPriorities(Paths.get("resources/Day_03.txt")));
     }
 
 
@@ -19,7 +17,19 @@ public class RucksackReorganization {
         List<String> input = Files.readAllLines(path);
         int sum = 0;
 
+        for (String rucksack : input) {
+            String firstComp = rucksack.substring(0, rucksack.length() / 2);
+            String secondComp = rucksack.substring(rucksack.length() / 2);
 
+            for (int i = 0; i < firstComp.length(); i++) {
+                if (secondComp.contains(firstComp.charAt(i) + "")) {
+                    if (firstComp.charAt(i) <= 'Z') sum += firstComp.charAt(i) - ('A' - 27);
+                    else sum += firstComp.charAt(i) - ('a' - 1);
+                    break;
+                }
+            }
+
+        }
         return sum;
     }
 
